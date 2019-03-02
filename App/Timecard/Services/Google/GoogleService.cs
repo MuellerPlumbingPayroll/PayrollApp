973bcc6ca0e10aa774dbd73151eb5f07aa903c34
bschweer <brian-schweer@uiowa.edu>
2019-03-01 18:39:17 -0600

    add backend for Google authentication functionality

---
+++ App/Timecard/Services/Google/GoogleService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Timecard.Services
+{
+    public class GoogleService
+    {
+        public async Task<GoogleUserInfo> GetUserInfoAsync(string tokenType, string accessToken)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(tokenType, accessToken);
+
+            var json = await httpClient.GetStringAsync("https://www.googleapis.com/oauth2/v1/userinfo?alt=json");
+            var userInfo = JsonConvert.DeserializeObject<GoogleUserInfo>(json);
+
+            return userInfo;
+        }
+    }
+}
---
+++ App/Timecard/Services/Google/GoogleUserInfo.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Timecard.Services
+{
+    public class GoogleUserInfo
+    {
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "given_name")]
+        public string GivenName { get; set; }
+
+        [JsonProperty(PropertyName = "family_name")]
+        public string FamilyName { get; set; }
+    }
+}
