a70c97fd4130b65bb602af810dab703c54e89d4e
bschweer <brian-schweer@uiowa.edu>
2019-03-26 13:22:54 -0500

    add auth view model and firebase user info classes

---
+++ App/Timecard/Models/FirebaseUserInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace Timecard.Models
+{
+    public class FirebaseUserInfo
+    {
+        public string Id { get; set; }
+
+        public async void SaveToDevice()
+        {
+            await SecureStorage.SetAsync("UserId", Id);
+        }
+
+        public static async Task<FirebaseUserInfo> ReadFromDevice()
+        {
+            try
+            {
+                var id = await SecureStorage.GetAsync("UserId");
+
+                if (id != null)
+                {
+                    return new FirebaseUserInfo()
+                    {
+                        Id = id
+                    };
+                }
+
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static void RemoveFromDevice()
+        {
+            SecureStorage.Remove("UserId");
+        }
+    }
+}
---
+++ App/Timecard/ViewModels/AuthViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Timecard.Models;
+using Timecard.Services;
+
+namespace Timecard.ViewModels
+{
+    public class AuthViewModel : BaseViewModel
+    {
+        public string FirebaseNotAuthorizedErrorMessage { get; set; }
+        private readonly GoogleService _googleService;
+
+        public AuthViewModel()
+        {
+            Title = "Sign In";
+            FirebaseNotAuthorizedErrorMessage = "You are not authorized to access the app. " 
+                + "If you believe this is an error, please contact your administrator.";
+
+            _googleService = new GoogleService();
+        }
+
+        public async Task<FirebaseUserInfo> AuthenticateUserWithFirebaseAsync(GoogleUserInfo googleUserInfo)
+        {
+            return await DataStore.AuthenticateUser(googleUserInfo);
+        }
+
+        public async Task<GoogleUserInfo> RetrieveGoogleUserInfoAsync(string tokenType, string accessToken)
+        {
+            return await _googleService.GetUserInfoAsync(tokenType, accessToken);
+        }
+
+        public async Task<bool> RevokeTokenAsync(string tokenType, string accessToken)
+        {
+            return await _googleService.RevokeTokenAsync(tokenType, accessToken);
+        }
+    }
+}
