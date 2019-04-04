bedd1f76dfb098c248c67e860ee9bf2d5391e672
bschweer <brian-schweer@uiowa.edu>
2019-04-03 21:19:25 -0500

    create base view controller class

---
+++ App/iOS/ViewControllers/BaseViewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UIKit;
+
+namespace Timecard.iOS.ViewControllers
+{
+    public class BaseViewController : UIViewController
+    {
+        public BaseViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public void DisplayAlertMessage(string message)
+        {
+            var alert = UIAlertController.Create("Error", message, UIAlertControllerStyle.Alert);
+            alert.AddAction(UIAlertAction.Create("Okay", UIAlertActionStyle.Cancel, null));
+
+            PresentViewController(alert, animated: true, completionHandler: null);
+        }
+    }
+}
