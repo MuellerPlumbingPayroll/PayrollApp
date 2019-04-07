98e68d2788db354e1c38a814f9946934b28bb352
bschweer <brian-schweer@uiowa.edu>
2019-04-07 11:38:04 -0500

    add profile scope to google configuration

--- App/iOS/GoogleConfiguration.cs
+++ App/iOS/GoogleConfiguration.cs
@@ -3,7 +3,7 @@
     public static class GoogleConfiguration
     {
         public const string ClientId = "717687086763-ktn15r665qgpb6a16f81qceibkesoer3.apps.googleusercontent.com";
-        public const string Scope = "email";
+        public const string Scope = "email profile";
         public const string RedirectUrl = "com.eduroam.timecard:/oauth2redirect";
     }
 }
